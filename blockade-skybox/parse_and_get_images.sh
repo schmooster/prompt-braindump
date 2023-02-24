f7ab9b4c4af3874ad4eb715049352c215abbe9b9
Gaz Collins <schmooster@gmail.com>
2023-02-24 02:47:39 +0000

    2023-02-24 adding a script to parse bloackadelabs output and download the images with a filename matching the interface uri

---
+++ blockade-skybox/parse_and_get_images.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+PROMPT_FILE=$1
+echo $PROMPT_FILE
+for LAB_URL in $(grep -i https $PROMPT_FILE|sed 's/.*\(https:\/\/\)/\1/'); do
+	echo $LAB_URL;
+	IMAGE_FILE="$(echo $LAB_URL|sed 's/.*\///').jpg";
+	echo $IMAGE_FILE;
+	URL=$(curl $LAB_URL | grep file_url | sed 's/.*file_url\\":\\"//' | sed 's/\\",\\"thumb_url.*//'|sed 's/\?.*//'); 
+	wget -o $IMAGE_FILE $URL
+done;
